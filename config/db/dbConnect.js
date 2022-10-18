dcfd16a0b407cdf759da0be68a785962150ee6d3
Aswathy N <aswathia.achu@gmail.com>
2022-10-18 15:23:44 +0530

    procfile added

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/devBlog",{
+console.log(process.env.MONGODB_ATLAS);
+mongoose.connect(process.env.MONGODB_ATLAS,{
     useNewUrlParser:true
 }).then(()=>{
 console.log("db connected")
